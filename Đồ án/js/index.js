0111bcf23bdd6c51e40daa9e0577cb1b4c53abf2
DoanPham <meodien.tb@gmail.com>
2020-02-07 11:52:32 +0700

    upload đồ án

---
+++ Đồ án/js/index.js
@@ -0,0 +1,16 @@
+// slick
+$(document).ready(function(){
+  $('.brands').slick({
+    mobileFirst:true,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+});
+$(document).ready(function(){
+  $('.feedback-item').slick({
+   slidesToShow: 3,
+   slidesToScroll:3,
+  });
+});
