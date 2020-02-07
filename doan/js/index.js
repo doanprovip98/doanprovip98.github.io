5a8b549ccdff23a5abd40eb37048af74fb34a4b5
DoanPham <meodien.tb@gmail.com>
2020-02-08 02:11:49 +0700

    create cartpage

--- doan/js/index.js
+++ doan/js/index.js
@@ -14,3 +14,33 @@ $(document).ready(function(){
    slidesToScroll:3,
   });
 });
+$('#form-pay').on('submit',function(){
+    var isValid=true;
+     if($('#name').val().trim()==''){
+       $('#name').next('span').text('Vui lòng nhập họ tên.');
+       isValid=false;
+     }
+     else{
+      $('#name').next('span').text('');
+     }
+
+     if($('#phone').val().trim()==''){
+      $('#phone').next('span').text('Vui lòng nhập số điện thoại.');
+      isValid=false;
+    }
+    else{
+     $('#phone').next('span').text('');
+    }
+
+    if($('#address').val().trim()==''){
+      $('#address').next('span').text('Vui lòng nhập địa chỉ.');
+      isValid=false;
+    }
+    else{
+     $('#phone').next('span').text('');
+    }
+    if(isValid==true){
+      alert("Bạn đã đặt hàng thành công!");
+    }
+    return isValid;
+});
