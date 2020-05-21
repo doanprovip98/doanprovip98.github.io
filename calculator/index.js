a60b906d500235f321172c4db92c121311261bc4
doanpham98 <meodien.tb@gmail.com>
2020-05-21 11:40:36 +0700

    update changetheme

--- calculator/index.js
+++ calculator/index.js
@@ -14,6 +14,12 @@ function rollback(){
     var exp=document.form.textview.value;
     document.form.textview.value = exp.substring(0,exp.length-1); 
 }
+var i=0;
 function changeTheme(){
-   
+    i++;
+    if(i%2==1){
+        document.getElementsByTagName('body')[0].style.background='linear-gradient(to right, #0f2027, #203a43, #2c5364)';
+    }
+    else
+    document.getElementsByTagName('body')[0].style.background='linear-gradient(to left, #cc95c0, #dbd4b4, #7aa1d2)';
 }
